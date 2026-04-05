ba92bdf2f30a8ad944af6ad3948dbeca3bebf040
xFireANTx <anton.schaff@gmail.com>
2026-04-05 18:57:22 +0200

    php

--- administrateur/serveur.php
+++ administrateur/serveur.php
@@ -1,3 +1,24 @@
 <?php
+
+    //accueil
+    $texte_barre_recherche = $_POST['texte_barre_recherche'];
+
+    //connexion
+    $connexion_adresse_mail = $_POST['connexion_Adresse_e-mail'];
+    $connexion_mdp = $_POST['connexion_mdp'];
+
+    //inscription
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prénom'];
+    $adresse = $_POST['adresse'];
+    $num_postale = $_POST['Num postale'];
+    $localite = $_POST['Localité'];
+    $phone = $_POST['phone'];
+    $adresse_mail = $_POST['Adresse e-mail'];
+    $mdp = $_POST['mdp'];
+    $confirmation_mdp = $_POST['confirmation mdp'];
+
+
     
+
 ?>
