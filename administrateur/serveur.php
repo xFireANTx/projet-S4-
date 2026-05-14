458a9840f2764a3ec0ec31f834d07aedd626197f
MoT186 <tommachefert78240@gmail.com>
2026-05-15 01:16:35 +0200

    Remove registration fields from serveur.php
    J'ai enlevé la partie inscription car elle est dans un fichier php à part dans utilisateur

--- administrateur/serveur.php
+++ administrateur/serveur.php
@@ -3,17 +3,6 @@
     //connexion
     $connexion_adresse_mail = $_POST['connexion_Adresse_e-mail'];
     $connexion_mdp = $_POST['connexion_mdp'];
-
-    //inscription
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prénom'];
-    $adresse = $_POST['adresse'];
-    $num_postale = $_POST['Num postale'];
-    $localite = $_POST['Localité'];
-    $phone = $_POST['phone'];
-    $adresse_mail = $_POST['Adresse e-mail'];
-    $mdp = $_POST['mdp'];
-    $confirmation_mdp = $_POST['confirmation mdp'];
         
     //acceuil
     $search = isset($_GET['search']) ? strtolower($_GET['search']) : '';
