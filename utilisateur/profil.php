4e1a5d0cc2ce13354fbaa673d2ace60d7cf72164
MoT186 <tommachefert78240@gmail.com>
2026-05-15 18:46:47 +0200

    Refactor profil.php with new HTML and meta tags
    Updated the profil.php file to include a new HTML structure and meta tags for the fictitious restaurant 'Japindien'.

---
+++ utilisateur/profil.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="utilisateur.css">
+	<meta name="author" content="groupe 6" />
+	<meta name="description" content="Cette page est la page d'acceuil du restaurant fictif le Japindien" />
+	<meta name="keywords" content="page accueil, restaurant, Japindien" />
+</head>
+
+<body>
+<h1>texte</h1>
+</body>
+</html>
